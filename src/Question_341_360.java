76a75237f033606a3257291075b8f3f7a0fd2bea
edmondxiexie <xiejy36@gmail.com>
2016-12-06 19:08:05 -0500

    406. Queue Reconstruction by Height

--- src/Question_341_360.java
+++ src/Question_341_360.java
@@ -1,3 +1,12 @@
 public class Question_341_360 {
 
+    /**
+     * 351. Android Unlock Patterns.
+     * @param m
+     * @param n
+     * @return
+     */
+    public int numberOfPatterns(int m, int n) {
+        return 0;
+    }
 }
