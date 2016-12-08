0e9e6433b5d90383ed1c37b9457793d799de0e98
edmondxiexie <xiejy36@gmail.com>
2016-12-08 17:38:12 -0500

    220. Contains Duplicate III

--- src/Question_201_220.java
+++ src/Question_201_220.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * Created by Edmond on 11/29/16.
  */
-public class Question_200_220 {
+public class Question_201_220 {
 
     /**
      * 204. Count Primes.
@@ -42,6 +44,35 @@ public class Question_200_220 {
             }
         }
         return true;
+    }
+
+    /**
+     * 220. Contains Duplicate III.
+     * @param nums
+     * @param k
+     * @param t
+     * @return
+     */
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        if (t < 0) {
+            return false;
+        }
+        SortedSet<Long> set = new TreeSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.size() > k) {
+                set.remove((long)nums[i - k - 1]);
+            }
+            int cur = nums[i];
+            SortedSet<Long> subSet = set.subSet((long)cur - t, (long)cur + t + 1);
+            if (!subSet.isEmpty()) {
+                return true;
+            }
+            set.add((long)cur);
+        }
+        return false;
     }
 
     public static void main(String[] args) {
