541b7e79a64d57473179fdbc9009f2951ebbde31
edmondxiexie <xiejy36@gmail.com>
2016-12-08 13:02:00 -0500

    156. Binary Tree Upside Down

--- src/Question_141_160.java
+++ src/Question_141_160.java
@@ -1,5 +1,21 @@
-/**
- * Created by Edmond on 12/3/16.
- */
 public class Question_141_160 {
+
+    /**
+     * 156. Binary Tree Upside Down.
+     * @param root
+     * @return
+     */
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if (root == null || root.left == null && root.right == null) {
+            return root;
+        }
+
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+
+        return newRoot;
+    }
 }
--- src/Question_401_420.java
+++ src/Question_401_420.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Question_401_420 {
     
@@ -28,4 +25,85 @@ public class Question_401_420 {
         }
         return result.toArray(new int[people.length][]);
     }
+
+    /**
+     * 417. Pacific Atlantic Water Flow.
+     * @param matrix
+     * @return
+     */
+    public List<int[]> pacificAtlantic(int[][] matrix) {
+        List<int[]> res = new ArrayList<>();
+        if (matrix.length == 0) {
+            return res;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        boolean[][] p = new boolean[rows][cols];
+        boolean[][] a = new boolean[rows][cols];
+
+        boolean[][] visited;
+        for (int i = 0; i < rows; i++) {
+            p[i][0] = true;
+            a[i][cols - 1] = true;
+            visited = new boolean[rows][cols];
+            DFSHelper(matrix, p, i, 0, visited);
+            visited = new boolean[rows][cols];
+            DFSHelper(matrix, a, i, cols - 1, visited);
+        }
+
+        for (int i = 0; i < cols; i++) {
+            p[0][i] = true;
+            a[rows - 1][i] = true;
+            visited = new boolean[rows][cols];
+            DFSHelper(matrix, p, 0, i, visited);
+            visited = new boolean[rows][cols];
+            DFSHelper(matrix, a, rows - 1, i, visited);
+        }
+
+        for(int i = 0; i < rows; i++)
+            for(int j = 0; j < cols; j++)
+                if(a[i][j] && p[i][j])
+                {
+                    int[] temp = new int[2];
+                    temp[0] = i;
+                    temp[1] = j;
+                    res.add(temp);
+                }
+        return res;
+    }
+
+    public void DFSHelper(int[][] matrix,boolean[][] map,int row, int col, boolean[][] visited) {
+
+        if (visited[row][col]) {
+            return;
+        }
+        visited[row][col] = true;
+        map[row][col] = true;
+        int cur = matrix[row][col];
+
+        if (row > 0 && matrix[row - 1][col] >= cur) {
+            DFSHelper(matrix, map, row - 1, col, visited);
+        }
+        if (col > 0 && matrix[row][col-1] >= cur) {
+            DFSHelper(matrix, map, row, col - 1, visited);
+        }
+        if (row < map.length-1 && matrix[row + 1][col] >= cur) {
+            DFSHelper(matrix, map, row + 1, col, visited);
+        }
+        if (col < map[0].length-1 && matrix[row][col + 1] >= cur) {
+            DFSHelper(matrix, map, row, col + 1, visited);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
