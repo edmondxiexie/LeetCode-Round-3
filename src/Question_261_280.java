ac76edb61f07074b786270d363e3c9692b974aa6
edmondxiexie <xiejy36@gmail.com>
2016-12-23 14:36:52 -0500

    278. First Bad Version.

--- src/Question_261_280.java
+++ src/Question_261_280.java
@@ -185,6 +185,31 @@ public class Question_261_280 {
     }
 
     /**
+     * 278. First Bad Version.
+     * @param n
+     * @return
+     */
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        int result = start;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) {
+                result = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return result;
+    }
+
+    private boolean isBadVersion(int n) {
+        return true;
+    }
+
+    /**
      * 280. Wiggle Sort.
      * @param nums
      */
