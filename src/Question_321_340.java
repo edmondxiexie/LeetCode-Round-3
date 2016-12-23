47d7370edc789b493181fd53c9cefc4970e5b80f
edmondxiexie <xiejy36@gmail.com>
2016-12-23 16:11:29 -0500

    339. Nested List Weight Sum.

---
+++ src/Question_321_340.java
@@ -0,0 +1,23 @@
+public class Question_321_340 {
+    /**
+     * 339. Nested List Weight Sum.
+     * @param nestedList
+     * @return
+     */
+//    public int depthSum(List<NestedInteger> nestedList) {
+//        int level = 1;
+//        return depthSum(nestedList, level);
+//    }
+//
+//    private int depthSum(List<NestedInteger> nestedList, int level) {
+//        int sum = 0;
+//        for (NestedInteger n : nestedList) {
+//            if (n.isInteger()) {
+//                sum += n.getInteger() * level;
+//            } else {
+//                sum += depthSum(n.getList(), level + 1);
+//            }
+//        }
+//        return sum;
+//    }
+}
