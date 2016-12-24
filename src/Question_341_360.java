53226b20eff44cd9068da49540fb403f62286b21
edmondxiexie <xiejy36@gmail.com>
2016-12-24 13:04:57 -0500

    350. Intersection of Two Arrays II

--- src/Question_341_360.java
+++ src/Question_341_360.java
@@ -48,6 +48,36 @@ public class Question_341_360 {
     }
 
     /**
+     * 350. Intersection of Two Arrays II.
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int p1 = 0;
+        int p2 = 0;
+        List<Integer> list = new ArrayList<>();
+        while (p1 < nums1.length && p2 < nums2.length) {
+            if (nums1[p1] < nums2[p2]) {
+                p1++;
+            } else if (nums2[p2] < nums1[p1]) {
+                p2++;
+            } else {
+                list.add(nums1[p1]);
+                p1++;
+                p2++;
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+
+    /**
      * 351. Android Unlock Patterns.
      * @param m
      * @param n
