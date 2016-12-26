074ae2f3cedc89b5bfd860ec092098d165fe4bc3
edmondxiexie <xiejy36@gmail.com>
2016-12-25 22:18:18 -0500

    400. Nth Digit.

--- src/Question_381_400.java
+++ src/Question_381_400.java
@@ -289,8 +289,33 @@ public class Question_381_400 {
 //        return -1.0;
 //    }
 
+    /**
+     * 400. Nth Digit.
+     * @param n
+     * @return
+     */
+    public static int findNthDigit(int n) {
+        //小心溢出
+        int digitType = 1;
+        long digitNum = 9;
+        //定位到是几位数
+        while (n > digitNum * digitType){
+            n -= (int) digitNum * digitType ;
+            digitType++;
+            digitNum *= 10;
+        }
+        //定位到是这些几位数里面的第几个的第几位
+        int indexInSubRange = (n - 1) / digitType;
+        int indexInNum = (n - 1) % digitType;
+        //还原数字
+        int num = (int) Math.pow(10, digitType - 1) + indexInSubRange;
+        int result = Integer.parseInt(("" + num).charAt(indexInNum)+ "");
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println(firstUniqChar("leetcode"));
+//        System.out.println(firstUniqChar("leetcode"));
 //        System.out.println();
+        System.out.println(findNthDigit(111));
     }
 }
