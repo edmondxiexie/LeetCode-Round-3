d9607c62057fc0618b66e6eb52d0614452307dc6
edmondxiexie <xiejy36@gmail.com>
2016-12-27 02:13:35 -0500

    412. Fizz Buzz.

--- src/Question_401_420.java
+++ src/Question_401_420.java
@@ -186,6 +186,27 @@ public class Question_401_420 {
     }
 
     /**
+     * 412. Fizz Buzz.
+     * @param n
+     * @return
+     */
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(i + "");
+            }
+        }
+        return result;
+    }
+
+    /**
      * 417. Pacific Atlantic Water Flow.
      * @param matrix
      * @return
