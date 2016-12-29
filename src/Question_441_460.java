a3d4b5793323f9764ac12c2c268b46970054f10b
edmondxiexie <xiejy36@gmail.com>
2016-12-29 13:21:40 -0500

    455. Assign Cookies.

--- src/Question_441_460.java
+++ src/Question_441_460.java
@@ -209,4 +209,28 @@ public class Question_441_460 {
         return total;
     }
 
+    /**
+     * 455. Assign Cookies.
+     * @param g
+     * @param s
+     * @return
+     */
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int gIndex = 0;
+        int sIndex = 0;
+        int count = 0;
+        while (gIndex < g.length && sIndex < s.length) {
+            if (g[gIndex] <= s[sIndex]) {
+                count++;
+                gIndex++;
+                sIndex++;
+            } else {
+                sIndex++;
+            }
+        }
+        return count;
+    }
+
 }
