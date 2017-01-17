efc6fdf7a0bbed0830b5b3bc790df494d1972566
edmondxiexie <xiejy36@gmail.com>
2017-01-17 16:52:38 -0500

    343. Integer Break.

--- src/Question_341_360.java
+++ src/Question_341_360.java
@@ -12,6 +12,25 @@ public class Question_341_360 {
     }
 
     /**
+     * 343. Integer Break.
+     * @param n
+     * @return
+     */
+    public int integerBreak(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for (int i = 4; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 2] * 2, dp[i - 3] * 3);
+        }
+        return dp[n];
+    }
+
+    /**
      * 349. Intersection of Two Arrays.
      * @param nums1
      * @param nums2
