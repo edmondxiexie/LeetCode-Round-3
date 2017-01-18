486cb80016e07addce2bdf238c42a12aeeea4c31
edmondxiexie <xiejy36@gmail.com>
2017-01-17 19:48:22 -0500

    334. Increasing Triplet Subsequence.

--- src/Question_321_340.java
+++ src/Question_321_340.java
@@ -61,6 +61,27 @@ public class Question_321_340 {
         return val;
     }
 
+    /**
+     * 334. Increasing Triplet Subsequence.
+     * @param nums
+     * @return
+     */
+    public boolean increasingTriplet(int[] nums) {
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    if (dp[i] >= 3) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
 //    /**
 //     * 339. Nested List Weight Sum.
 //     * @param nestedList
