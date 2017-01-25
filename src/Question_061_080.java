a5966c38c6efcbdca11d2c063029fcc8b50d4fc3
edmondxiexie <xiejy36@gmail.com>
2017-01-24 22:14:40 -0500

    71. Simplify Path.

--- src/Question_061_080.java
+++ src/Question_061_080.java
@@ -1,8 +1,46 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class Question_061_080 {
+
+    /**
+     * 71. Simplify Path.
+     * @param path
+     * @return
+     */
+    public String simplifyPath(String path) {
+        Stack<String> stack=new Stack<> ();
+        while (path.length() > 0){
+            int start = path.indexOf("/"); //第一个斜杠的位置
+            path = path.substring(start+1); //Path一进被截取
+            int end = path.indexOf("/");//第二个斜杠的位置
+            if (end == -1)
+                end = path.length();
+
+            String part = path.substring(0, end);
+            path = path.substring(end);
+
+            // System.out.println(part+"  "+path);
+            if (part.equals(".") || part.equals(""))
+                continue;
+            if (part.equals("..")) {
+                if (!stack.isEmpty())
+                    stack.pop();
+            }
+
+            else stack.push("/"+part);
+        }
+        String result = "";
+        while (!stack.isEmpty()){
+            result = stack.pop() + result;
+        }
+        if (result.length() == 0)
+            result = "/";
+        return result;
+    }
+
     /**
      * 77. Combinations.
      * @param n
